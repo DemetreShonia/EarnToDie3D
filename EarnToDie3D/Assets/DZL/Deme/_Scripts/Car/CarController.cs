5f3b722227ac3466736593330ac15331bf17aa21
DemetreShonia <shonka.demeloper@gmail.com>
2023-07-18 15:02:17 +0400

    Refactoring, Works Ok

--- EarnToDie3D/Assets/DZL/Deme/_Scripts/Car/CarController.cs
+++ EarnToDie3D/Assets/DZL/Deme/_Scripts/Car/CarController.cs
@@ -5,30 +5,11 @@ using System.Xml.Serialization;
 
 namespace DumbRide
 {
-    public enum Axle
-    {
-        Front,
-        Rear
-    }
-    public enum DriveType
-    {
-        FrontWheelDrive,
-        RearWheelDrive,
-        AllWheelDrive
-    }
-
-    [Serializable]
-    public struct Wheel
-    {
-        public GameObject wheelModel;
-        public WheelCollider wheelCollider;
-        public Axle axel;
-    }
 
     public class CarController : MonoBehaviour
     {
+        [SerializeField] GearBox _gearBox; 
         [Header("Car Properties")]
-        [SerializeField] DriveType _driveType = DriveType.RearWheelDrive;
         [SerializeField]
         [Tooltip("Percent of velocity")]
         [Range(0.1f, 0.5f)] float _downPressurePercent = 0.2f;
@@ -67,9 +48,9 @@ namespace DumbRide
         }
         void FixedUpdate()
         {
-            ApplyForces();
-            SteerFrontWheels();
+            SteerAckerman();
             TryBrake();
+            _gearBox.ApplyMotorTorque(_maxAcceleration, _moveInput);
         }
         void CheckInputs()
         {
@@ -78,63 +59,16 @@ namespace DumbRide
         }
 
 
-        // MOVE THIS TO GearBox Script
-        void ApplyForces()
-        {
-            // make down pressure 20% of the velocity
-            //_downPressure = Mathf.Lerp(_downPressure, _carRb.velocity.magnitude * _downPressurePercent, Time.fixedDeltaTime);
-            //_carRb.AddForce(-transform.up * _downPressure, ForceMode.Force);
-            float _currentTorque = _maxAcceleration * _moveInput;
-
-            void ApplyBothAxles()
-            {
-                _currentTorque /= 4; // distribute to 4 wheels
-
-                foreach (var wheel in _wheels)
-                {
-                    wheel.wheelCollider.motorTorque = _currentTorque;
-                }
-            }
-
-            void ApplySingleAxle(Axle axel)
-            {
-                _currentTorque /= 2; // distribute to 2 wheels
-
-                foreach (var wheel in _wheels)
-                {
-                    if(wheel.axel == axel)
-                        wheel.wheelCollider.motorTorque = _currentTorque;
-                }
-            }
-            switch (_driveType)
-            {
-                case DriveType.AllWheelDrive:
-                    ApplyBothAxles();
-                    break;
-                case DriveType.FrontWheelDrive:
-                    ApplySingleAxle(Axle.Front);
-                    break;
-                case DriveType.RearWheelDrive:
-                    ApplySingleAxle(Axle.Rear);
-                    break;
-            }
-        }
         // steer properties
         [SerializeField] float _wheelBase = 4f;
         [SerializeField] float _rearTrack = 2.5f;
         [SerializeField] float _turnRadius = 5.0f;
-        void SteerFrontWheels()
+        void SteerAckerman()
         {
-            // ackerman steer formula
-            // Wheelbase = distance between front and rear axles
-            // Reartrack = distance between rear wheels
-            // TurnRadius = radius of the turn
-            //var steerAngle = Mathf.Rad2Deg * Mathf.Atan(_wheelBase / (_turnRadius + (_rearTrack / 2))) * _steerInput;
-            
             float ackermanLeft = 0f;
             float ackermanRight = 0f;
 
-            if(_steerInput > 0f)
+            if (_steerInput > 0f)
             {
                 ackermanLeft = Mathf.Rad2Deg * Mathf.Atan(_wheelBase / (_turnRadius + (_rearTrack / 2))) * _steerInput;
                 ackermanRight = Mathf.Rad2Deg * Mathf.Atan(_wheelBase / (_turnRadius - (_rearTrack / 2))) * _steerInput;
@@ -145,19 +79,16 @@ namespace DumbRide
                 ackermanRight = Mathf.Rad2Deg * Mathf.Atan(_wheelBase / (_turnRadius + (_rearTrack / 2))) * _steerInput;
             }
 
-            // refactor this!
-            int id = 0;
             foreach (var wheel in _wheels)
             {
-                if(id == 2)
-                    break;
-                if (wheel.axel == Axle.Front)
+                switch (wheel.CarWheelType)
                 {
-                    if (id == 0)
-                        wheel.wheelCollider.steerAngle = ackermanLeft;
-                    else if (id == 1)
-                        wheel.wheelCollider.steerAngle = ackermanRight;
-                    id++;
+                    case Wheel.WheelType.FrontLeft:
+                        wheel.ApplySteerAngle(ackermanLeft);
+                        break;
+                    case Wheel.WheelType.FrontRight:
+                        wheel.ApplySteerAngle(ackermanRight);
+                        break;
                 }
             }
         }
@@ -165,18 +96,14 @@ namespace DumbRide
         void TryBrake()
         {
             foreach (var wheel in _wheels)
-                wheel.wheelCollider.brakeTorque = (Input.GetKey(KeyCode.Space) || _moveInput == 0f) ? _brakeAcceleration : 0;
+                wheel.ApplyBrakeTorque((Input.GetKey(KeyCode.Space) || _moveInput == 0f) ? _brakeAcceleration : 0);
         }
 
         void AnimateWheels()
         {
             foreach (var wheel in _wheels)
             {
-                Quaternion rot;
-                Vector3 pos;
-                wheel.wheelCollider.GetWorldPose(out pos, out rot);
-                wheel.wheelModel.transform.position = pos;
-                wheel.wheelModel.transform.rotation = rot;
+                wheel.AnimateWheel();
             }
         }
     }
--- EarnToDie3D/Assets/DZL/Deme/_Scripts/Car/GearBox.cs
+++ EarnToDie3D/Assets/DZL/Deme/_Scripts/Car/GearBox.cs
@@ -1,22 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace DumbRide
 {
+    public enum Axle
+    {
+        None,
+        Front,
+        Rear
+    }
     public class GearBox : MonoBehaviour
     {
-        public enum Axle
-        {
-            Front,
-            Rear
-        }
         public enum DriveType
         {
             FrontWheelDrive,
             RearWheelDrive,
             AllWheelDrive
         }
-        
+
+        [SerializeField] DriveType _driveType = DriveType.AllWheelDrive;
+        [SerializeField] Wheel[] _wheels;
+
+        public void ApplyMotorTorque(float _maxAcceleration, float _moveInput)
+        {
+            float maxTorq = _maxAcceleration * _moveInput;
+            int wheelCount = _wheels.Length;
+
+            // distribute torque to wheels, case 4 or case 2
+            float currentTorque = _driveType == DriveType.AllWheelDrive ? maxTorq / wheelCount : maxTorq * 0.5f;
+
+            foreach (var wheel in _wheels)
+            {
+                switch (_driveType)
+                {
+                    case DriveType.AllWheelDrive:
+                        wheel.ApplyMotorTorque(currentTorque);
+                        break;
+                    case DriveType.FrontWheelDrive:
+                        if (wheel.WheelAxle == Axle.Front)
+                            wheel.ApplyMotorTorque(currentTorque);
+                        break;
+                    case DriveType.RearWheelDrive:
+                        if (wheel.WheelAxle == Axle.Rear)
+                            wheel.ApplyMotorTorque(currentTorque);
+                        break;
+                }
+            }
+        }
     }
 }
