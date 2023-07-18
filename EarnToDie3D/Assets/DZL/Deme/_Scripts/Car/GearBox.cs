cd94bfa6c88f529e6b3c85d655256e34d42b00bd
DemetreShonia <shonka.demeloper@gmail.com>
2023-07-18 14:01:26 +0400

    Before Refactor

---
+++ EarnToDie3D/Assets/DZL/Deme/_Scripts/Car/GearBox.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DumbRide
+{
+    public class GearBox : MonoBehaviour
+    {
+        public enum Axle
+        {
+            Front,
+            Rear
+        }
+        public enum DriveType
+        {
+            FrontWheelDrive,
+            RearWheelDrive,
+            AllWheelDrive
+        }
+        
+    }
+}
